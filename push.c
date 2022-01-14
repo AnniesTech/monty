2a5eb48e8de755a37d55c085b7009ddf39ceede1
AnniesTech <anita.rocha0505@gmail.com>
2022-01-14 11:44:11 -0500

    executable and push

---
+++ push.c
@@ -0,0 +1,32 @@
+#include "monty.h"
+
+/**
+ * push - adds a new node
+ * @stack: doubly linked list
+ * @new_number: integer that will be added
+ * Return: void
+ */
+
+void push(stack_t **stack, unsigned int new_number)
+{
+	stack_t *new = NULL, *temp = NULL;
+
+	if (stack)
+	{
+		new = malloc(sizeof(stack_t));
+		if (!new)
+		{
+			free(new);
+			return;
+		}
+		temp = *stack;
+		new->prev = NULL;
+		new->next = temp;
+		new->n = new_number;
+		if (new->next)
+		{
+			temp->prev = new;
+		}
+		*stack = new;
+	}
+}
---
+++ start.c
@@ -0,0 +1,70 @@
+#include "monty.h"
+
+/**
+ * start - Function that selects the function to execute it
+ * @stack: linked list
+ * @name: Name of the function
+ * @value: The number in the file
+ * @inf: The number of the line
+ *
+ * Return: nothing
+ */
+
+void start(stack_t **stack, char *name, char *value, int inf)
+{
+	int indx = 0;
+
+	for (; instructions[indx].opcode; indx++)
+	{
+		if (strcmp(name, instructions[indx].opcode) == 0)
+		{
+			if (strcmp(name, instructions[0].opcode) != 0)
+			{
+				instructions[indx].f(stack, inf);
+				return;
+			}
+			if (strcmp(name, instructions[0].opcode) == 0 && !_checknum(value))
+			{
+				status = EXIT_FAILURE;
+				fprintf(stderr, "L%d: usage: push integer\n", inf);
+				return;
+			}
+			if (strcmp(name, instructions[0].opcode) == 0 && !value)
+			{
+				status = EXIT_FAILURE;
+				fprintf(stderr, "L%d: usage: push integer\n", inf);
+				return;
+			}
+			instructions[indx].f(stack, atoi(value));
+			return;
+		}
+	}
+	status = EXIT_FAILURE;
+	fprintf(stderr, "L%d: unknown instruction %s\n", inf, name);
+}
+
+/**
+ * _isnumber - checks if a string is a number
+ * @str: provided string
+ *
+ * Return: 1 if the string is a number, else, 0.
+ */
+int _checknum(char *str)
+{
+	int i = 0;
+
+	if (!str)
+		return (0);
+	if (str[i] == '-')
+	{
+		i++;
+	}
+	for (; str[i]; i++)
+	{
+		if (str[i] < '0' || str[i] > '9')
+		{
+			return (0);
+		}
+	}
+	return (1);
+}
